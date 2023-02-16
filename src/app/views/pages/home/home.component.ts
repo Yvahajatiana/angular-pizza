4950943143183b9a11e719633dcc5e52808734a4
yva <yvahajatiana@gmail.com>
2023-02-16 15:49:17 +0100

    feat: move carousel function to home

--- src/app/views/pages/home/home.component.ts
+++ src/app/views/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+declare var $: any;
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,10 +11,66 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const carousel = function () {
+      $('.home-slider').owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: false,
+        autoplayHoverPause: false,
+        items: 1,
+        navText: [
+          "<span class='ion-md-arrow-back'></span>",
+          "<span class='ion-chevron-right'></span>"
+        ],
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          600: {
+            items: 1,
+            nav: false
+          },
+          1000: {
+            items: 1,
+            nav: false
+          }
+        }
+      });
+      $('.carousel-work').owlCarousel({
+        autoplay: true,
+        center: true,
+        loop: true,
+        items: 1,
+        margin: 30,
+        stagePadding: 0,
+        nav: true,
+        navText: [
+          '<span class="ion-ios-arrow-back">',
+          '<span class="ion-ios-arrow-forward">'
+        ],
+        responsive: {
+          0: {
+            items: 1,
+            stagePadding: 0
+          },
+          600: {
+            items: 2,
+            stagePadding: 50
+          },
+          1000: {
+            items: 3,
+            stagePadding: 100
+          }
+        }
+      });
+    };
+    carousel();
   }
-
 }
